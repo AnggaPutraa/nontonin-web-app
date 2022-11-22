aa8ddeab40dc3488ee01cfdef882018c20e1ca1b
AnggaPutraa <yananggaa@gmail.com>
2022-11-22 20:08:50 +0700

    refactor: navbar components

---
+++ components/Header.tsx
@@ -0,0 +1,52 @@
+import Link from 'next/link';
+import { BellIcon, MagnifyingGlassIcon } from '@heroicons/react/24/solid';
+import { useEffect, useState } from 'react';
+
+const Header = () => {
+    const [isScorll, setIsScroll] = useState(false);
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 0) {
+                setIsScroll(true);
+            } else {
+                setIsScroll(false);
+            }
+        }
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        }
+    }, []);
+
+    return (
+        <div>
+            <header>
+                <div className="flex items-center space-x-2 md:space-x-6">
+                    <h2 className='text-4xl font-bold text-white'>NONTONIN</h2>
+                    <ul className="hidden space-x-4 md:flex">
+                        <li className="header-item-link">Home</li>
+                        <li className="header-item-link">TV Shows</li>
+                        <li className="header-item-link">Movies</li>
+                        <li className="header-item-link">New & Popular</li>
+                        <li className="header-item-link">My List</li>
+                    </ul>
+                </div>
+
+                <div className='flex items-center space-x-4'>
+                    <MagnifyingGlassIcon className='hidden sm:inline w-6 h-6' />
+                    <p className='hidden lg:inline text-sm'>Kids</p>
+                    <BellIcon className='h-6 w-6' />
+                    <Link href={"/account"}>
+                        <img
+                            src="https://rb.gy/g1pwyx"
+                            alt=""
+                            className="cursor-pointer rounded"
+                        />
+                    </Link>
+                </div>
+            </header>
+        </div>
+    );
+}
+
+export default Header;
